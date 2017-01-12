3344093269b268455e0a7329e5f9b3b5ed02ba81
shahmir811 <shahmirkj@gmail.com>
2017-01-12 14:39:10 +0500

    3-Creating the Authy Service

---
+++ app/Services/Authy/AuthyService.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Services\Authy;
+
+use App\User;
+use Authy\AuthyApi;
+use App\Services\Authy\Exceptions\InvalidTokenException;
+use App\Services\Authy\Exceptions\SmsRequestFailedException;
+use App\Services\Authy\Exceptions\RegistrationFailedException;
+
+class AuthyService
+{
+
+  private $client;
+
+  public class __construct(AuthyApi $client)
+  {
+    $this->client = $client;
+  }
+
+
+  public function registerUser(User $user)
+  {
+      $user = $this->client->registerUser(
+          $user->email,
+          $user->phoneNumber->phone_number,
+          $user->phoneNumber->diallingCode->dialling_code
+      );
+
+
+      if(!$user->ok()){
+        throw new RegistrationFailedException;
+      }
+
+      return $user->id();
+
+  }
+
+
+  public function verifyToken($token, User $user = null)
+  {
+      try {
+        $verification = $this->client->verifyToken(
+          $user ? $user->authy_id : request()->session()->get('authy.authy_id'),
+          $token
+        );
+
+      } catch(AuthyFormatException $e) {
+        throw new InvalidTokenException;
+      }
+
+      if(!$verification->ok())
+      {
+        throw new InvalidTokenException;
+      }
+
+      return true;
+  }
+
+  public function requestSms(User $user)
+  {
+    $request = $this->cient->requestSms($user->authy_id, [
+      'force' => true,
+    ]);
+
+    if(!$request->ok())
+    {
+        throw new SmsRequestFailedException;
+    }
+
+
+  }
+
+}
---
+++ app/Services/Authy/Exceptions/RegistrationFailedException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\Authy\Exceptions;
+
+use Exception;
+
+
+class RegistrationFailedException extends Exception
+{
+
+  function __construct(argument)
+  {
+    # code...
+  }
+
+}
